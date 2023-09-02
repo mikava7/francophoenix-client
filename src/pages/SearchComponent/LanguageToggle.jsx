dd8fbd6b52823f93892280e2676b68517dc7380f
Irakli Mikava <i.mikava365@gmail.com>
2023-09-02 10:37:47 +0200

    toggle input language

--- src/pages/SearchComponent/LanguageToggle.jsx
+++ src/pages/SearchComponent/LanguageToggle.jsx
@@ -1,11 +1,87 @@
 import React from "react";
+import styled, { keyframes, css } from "styled-components";
+import { slideLeft, slideRight } from "../../Styles/frames";
+// Import the flag emojis as React components (you can replace them with the actual flag emojis)
+import FrenchFlag from "../../../public/icons/french-flag-48.png";
+import GeorgianFlag from "../../../public/icons/georgia-48.png";
+import EnglishFlag from "../../../public/icons/usa-flag-48.png";
+import { useTranslation } from "react-i18next";
 
-const LanguageToggle = ({ inputLanguage, toggleInputLanguage }) => {
+const LanguageToggle = ({
+  inputLanguage,
+  toggleInputLanguage,
+  isGeorgian,
+  secondLanguage,
+}) => {
+  const { t } = useTranslation();
   return (
-    <button onClick={toggleInputLanguage}>
-      Switch to {inputLanguage === "french" ? "Second Language" : "French"}
-    </button>
+    <ToggleContainer>
+      <French onClick={toggleInputLanguage}>{t("Français")}</French>
+      <ToggleContainer onClick={toggleInputLanguage}>
+        <ToggleIcon inputLanguage={inputLanguage}>
+          <img
+            src={
+              inputLanguage === "french"
+                ? FrenchFlag
+                : isGeorgian
+                ? GeorgianFlag
+                : EnglishFlag
+            }
+            alt="FrenchFlag"
+          />
+        </ToggleIcon>
+      </ToggleContainer>
+      <SecondLanguage onClick={toggleInputLanguage}>
+        {isGeorgian ? "ქართული" : "English"}
+      </SecondLanguage>
+    </ToggleContainer>
   );
 };
 
 export default LanguageToggle;
+
+const ToggleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  width: 50%;
+`;
+
+const ToggleIcon = styled.div`
+  display: flex;
+  width: 4rem;
+  height: 2rem;
+  padding: 0.4rem;
+  background-color: ${(props) => (props.isGeorgian ? "#E0E0E0" : "#007bff")};
+  border-radius: 100px;
+  cursor: pointer;
+  &:not(input) {
+    user-select: none;
+  }
+  img {
+    width: 1.6rem;
+    @media (min-width: 576px) and (max-width: 767px) {
+      width: 1.4rem;
+    }
+    @media (max-width: 576px) {
+      width: 1.3rem;
+    }
+    animation: ${(props) =>
+      props.inputLanguage === "french"
+        ? css`
+            ${slideLeft} 0.8s forwards
+          `
+        : css`
+            ${slideRight} 0.8s forwards
+          `};
+  }
+`;
+const French = styled.div`
+  margin: 0.5rem auto;
+  outline: 2px solid ${(props) => props.theme.tertiaryText};
+  padding: 0.5rem 2rem;
+  border-radius: 12px;
+  width: 8rem;
+`;
+const SecondLanguage = styled(French)``;
