5ab494b1ccd19f04c231aff6fec45b9cc21ec4ba
Irakli Mikava <i.mikava365@gmail.com>
2023-09-04 21:50:49 +0200

    url changes

--- src/redux/api/axiosInstance.js
+++ src/redux/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://francophoenix.onrender.com/",
+  baseURL: "https://francophoenix.onrender.com",
 });
 
 export default instance;
