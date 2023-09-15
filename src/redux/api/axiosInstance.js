d2c38318f9ab3431d71c6a93b71f904a4e26d710
Irakli Mikava <i.mikava365@gmail.com>
2023-09-15 10:39:15 +0200

    url

--- src/redux/api/axiosInstance.js
+++ src/redux/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://francophoenix.onrender.com",
-  baseURL: "http://localhost:5500",
+  baseURL: "https://francophoenix.onrender.com",
+  // baseURL: "http://localhost:5500",
 });
 
 export default instance;
